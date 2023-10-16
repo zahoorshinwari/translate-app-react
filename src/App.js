bef5af1ca699888a301b316f9ed0373c0db67a0a
zahoorshinwari <khanzshinwari5371@gmail.com>
2023-10-16 22:49:06 +0500

    create the translate components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 import Field from './components/Field';
 import Languages from './components/Languages';
+import Translate from './components/translate';
 
 function App() {
+  const [text, setText] = useState("es")
+  const [language, setLanguage] = useState("")
   return (
     <div >
    
-    <Field />
-    <Languages />
+    <Field onChange={setText}/>
+    <Languages language={language} onLanguageChange={setLanguage}/>
+    <hr />
+    <Translate text={text} language={language}/>
     </div>
   );
 }
